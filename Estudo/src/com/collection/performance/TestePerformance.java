2be799dc915f7c35a48e41ef2fa9825317ae53b1
JUPITER\gustavo.ventura <gustavo.ventura@jupiter.co.ao>
2017-06-01 17:51:17 +0100

    contabilizando tempo em milesegundos para adição e verificação de cada classe da interface collection.

--- Estudo/src/com/collection/performance/TestePerformance.java
+++ Estudo/src/com/collection/performance/TestePerformance.java
@@ -1,18 +1,37 @@
 package com.collection.performance;
 
 import java.util.Collection;
-import java.util.TreeSet;
+import java.util.HashSet;
 
 public class TestePerformance {
 	public static void main(String[] args) {
 		System.out.println("Iniciando...");
+		/* tempo médio para adicionar - 12ms 
+		 * total 15ms*/
+		Collection<Integer> teste = new HashSet<>(); //Interface Set
 		
-		//Collection<Integer> teste = new HashSet<>(); //Interface Set
+		/* tempo médio para adicionar - 14ms 
+		 * total 15ms*/
 		//Collection<Integer> teste = new LinkedHashSet<>(); //Interface Set
-		Collection<Integer> teste = new TreeSet<>(); //Interface Setm SortedSet
+		
+		/* tempo médio para adicionar - 16ms 
+		 * total 26ms*/
+		//Collection<Integer> teste = new TreeSet<>(); //Interface Setm SortedSet
+		
+		/* tempo médio para adicionar - 3ms 
+		 * total 1109ms*/
 		//Collection<Integer> teste = new ArrayList<>(); //Interface List
+		
+		/* tempo médio para adicionar - 4ms 
+		 * total 1130ms*/
 		//Collection<Integer> teste = new Vector<>(); //Interface List
+		
+		/* tempo médio para adicionar - 4ms 
+		 * total 3311ms*/
 		//Collection<Integer> teste = new LinkedList<>(); //Interface List, Queue
+		
+		/* tempo médio para adicionar - 6ms 
+		 * total 1099ms*/
 		//Collection<Integer> teste = new PriorityQueue<>(); //Interface Queue
 		
 		//Map<Integer, String> teste = new Hashtable<Integer, String>(); // Interface map
@@ -23,7 +42,7 @@ public class TestePerformance {
 		
 		long inicio = System.currentTimeMillis();
 
-		int total = 30000;
+		int total = 50000;
 
 		for (int i = 0; i < total; i++) {
 			teste.add(i);
