42663e9c137b58b074f9edeba4f77ebf9af0a431
JUPITER\gustavo.ventura <gustavo.ventura@jupiter.co.ao>
2017-06-01 17:30:42 +0100

    Adicionando tipos de Collection para validar performance

--- Estudo/src/com/collection/performance/TestePerformance.java
+++ Estudo/src/com/collection/performance/TestePerformance.java
@@ -1,13 +1,26 @@
 package com.collection.performance;
 
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.TreeSet;
 
 public class TestePerformance {
 	public static void main(String[] args) {
 		System.out.println("Iniciando...");
-		Collection<Integer> teste = new ArrayList<>();
-		//Collection<Integer> teste = new HashSet<>();
+		
+		//Collection<Integer> teste = new HashSet<>(); //Interface Set
+		//Collection<Integer> teste = new LinkedHashSet<>(); //Interface Set
+		Collection<Integer> teste = new TreeSet<>(); //Interface Setm SortedSet
+		//Collection<Integer> teste = new ArrayList<>(); //Interface List
+		//Collection<Integer> teste = new Vector<>(); //Interface List
+		//Collection<Integer> teste = new LinkedList<>(); //Interface List, Queue
+		//Collection<Integer> teste = new PriorityQueue<>(); //Interface Queue
+		
+		//Map<Integer, String> teste = new Hashtable<Integer, String>(); // Interface map
+		//Map<Integer, String> teste = new LinkedHashMap<Integer, String>(); // Interface map
+		//Map<Integer, String> teste = new HashMap<Integer, String>(); // Interface map
+		//Map<Integer, String> teste = new TreeMap<Integer, String>(); // Interface map, SortedMap
+		
+		
 		long inicio = System.currentTimeMillis();
 
 		int total = 30000;
